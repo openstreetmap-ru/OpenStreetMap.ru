c331d5aabefa52c4df5c2c1834651003ce3730d4
Larry0ua <vpekar@.softservecom.com>
2011-12-22 14:09:44 +0200

    Fix for markers - auto open popup with url

--- www/js/markers.js
+++ www/js/markers.js
@@ -15,5 +15,5 @@ osm.markers.createPoint = function(e) {
 	url = url.replace(/lon=\d*(.?\d*)/, 'lon='+e.latlng.lng);
 	url += '&marker=1';
 	//http://openstreetmap.ru/#layer=Mapnik&zoom=7&lat=48.305&lon=27.11&marker=1
-	marker.bindPopup('<a href="'+url+'">Ссылка на маркер</a>');
+	marker.bindPopup('<a href="'+url+'">Ссылка на маркер</a>').openPopup();
 }
