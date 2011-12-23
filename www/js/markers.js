cb0486d2cc8ad759fb94bba6f06679dcc5fe75f9
Larry0ua <pekarskiy.valeriy@gmail.com>
2011-12-23 22:14:00 +0200

    Markers, paths... - UI

--- www/js/markers.js
+++ www/js/markers.js
@@ -1,9 +1,85 @@
+osm.markers = {
+	_drawingMode: 0,// 0 - nothing, 1 - marker (single, permalink), 2 - multimarker, 3 - line
+	_layerGroup: 0,
+	_newPath: 0
+}
 osm.markers.addPoint = function () {
+	if (osm.markers._removeHandlers() === 1)
+		return;
+
 	osm.map.on('click', osm.markers.createPoint);
 	$_('map').style.cursor = 'crosshair';
+	osm.markers._drawingMode = 1;
 }
 osm.markers.createPoint = function(e) {
 	osm.map.permalink._popup_marker(e.latlng);
 	osm.map.off('click', osm.markers.createPoint);
 	$_('map').style.cursor='';
+}
+osm.markers._removeHandlers = function() {
+	var oldDrawingMode = osm.markers._drawingMode;
+	var func, elementId;
+	switch(osm.markers._drawingMode){
+	case 1:
+			func = osm.markers.createPoint;
+			elementId = '';
+			break;
+	case 2:
+			func = osm.markers.createPoints;
+			elementId = 'multimarkerbutton';
+			break;
+	case 3:
+			func = osm.markers.createPath;
+			elementId = 'pathbutton';
+			break;
+	default:
+		return 0;
+	}
+	osm.map.off('click', func);
+	if (elementId)
+		$(elementId).className = 'map-feature-button';
+	$('map').style.cursor='';
+	osm.markers._drawingMode = 0;
+	return oldDrawingMode;
+}
+
+osm.markers.addMultiMarker = function() {
+	if (osm.markers._removeHandlers() === 2)
+		return;
+
+	osm.map.on('click', osm.markers.createPoints);
+	$('multimarkerbutton').className += ' map-feature-button-pressed';
+	$('map').style.cursor = 'crosshair';
+	osm.markers._drawingMode = 2;
+
+	if (osm.markers._layerGroup === 0) {
+		osm.markers._layerGroup = new L.LayerGroup();
+		osm.map.addLayer(osm.markers._layerGroup);
+	}
+}
+osm.markers.createPoints = function(e) {
+	var marker = new L.Marker(e.latlng);
+	osm.markers._layerGroup.addLayer(marker);
+}
+
+osm.markers.addPath = function() {
+	if (osm.markers._removeHandlers() === 3)
+		return;
+
+	osm.map.on('click', osm.markers.createPath);
+	$('pathbutton').className += ' map-feature-button-pressed';
+	$('map').style.cursor = 'crosshair';
+	osm.markers._drawingMode = 3;
+	osm.markers._newPath = new L.Polyline([]);
+	osm.map.addLayer(osm.markers._newPath);
+}
+osm.markers.createPath = function(e) {
+	if (osm.markers._newPath.getLatLngs().length === 0) {
+		var marker = new L.Marker(e.latlng);
+		osm.map.addLayer(marker);
+		marker.bindPopup('Начальная точка');
+		marker.openPopup();
+		
+	}
+	osm.markers._newPath.addLatLng(e.latlng);
 }
