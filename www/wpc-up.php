6f3805d59d6280bfec49dbd71fc17ed439806992
Alexandr Zeinalov <shurik@sbin.ru>
2012-01-08 15:41:18 +0400

    Validating args in wpc-up.php

--- www/wpc-up.php
+++ www/wpc-up.php
@@ -2,13 +2,21 @@
 include_once("include/passwd.php");
 pg_connect($pgconnstr);
 
+Header("Content-Type: text/html; charset=utf-8");
+
 if (!isset($_GET["title"])) {
-  Header("Content-Type: text/html; charset=utf-8");
   print "Обязательный параметр title не определён!";
   exit;
 }
 
 $title = $_GET["title"];
+if (isset($_GET["js"]) && $_GET["js"]) $title = urldecode($title);
+$title = preg_replace("@^http://commons\.wikimedia\.org/wiki/@", "", $title);
+$title = preg_replace("@\?.+$@", "", $title);
+if (!preg_match("@^File:@", $title)) {
+  print "Страница <b>$title</b> не является страницей описания картинки";
+  exit;
+}
 
 $query = "INSERT INTO wpc_req (page, added) VALUES ('".pg_escape_string($title)."', NOW())";
 $res = pg_query($query);
