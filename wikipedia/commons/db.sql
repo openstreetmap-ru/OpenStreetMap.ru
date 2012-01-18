b91206b36e02bbcb93806355412b52b4061e90d3
Alexandr Zeinalov <shurik@sbin.ru>
2012-01-18 21:54:38 +0400

    Add timestamps to wpc tables

--- wikipedia/commons/db.sql
+++ wikipedia/commons/db.sql
@@ -1,11 +1,19 @@
 create table wpc_img (
   page text not null unique,
   "desc" text not null default '',
-  point geometry
+  point geometry,
+  date timestamp with time zone not null
 );
 create table wpc_req (
   page text not null unique,
   added timestamp with time zone
+);
+create table wpc_done (
+  page text not null,
+  "desc" text not null default '',
+  lat float,
+  lon float,
+  done timestamp with time zone
 );
 
 -- Description of FIRST aggregate function:
--- www/wpc-queue.php
+++ www/wpc-queue.php
@@ -8,13 +8,24 @@ $res = pg_query($query);
 Header("Content-Type: text/html; charset=utf-8");
 
 if (!pg_num_rows($res)) {
-  print "Очередь пуста";
-  exit;
+  print "<p>Очередь пуста</p>";
+} else {
+  print "Очередь<p>\n<table border=\"1\" cellspacing=\"0\">\n<tr><th>Страница</th><th>Дата</th></tr>\n";
+  while ($row = pg_fetch_assoc($res)) {
+    print "<tr><td>".htmlentities($row["page"],ENT_COMPAT,"utf-8")."</td><td>".$row["added"]."</td></tr>\n";
+  }
+  print "</table>";
 }
 
-print "Очередь<p>\n<table border=\"1\" cellspacing=\"0\">\n<tr><th>Страница</th><th>Дата</th></tr>\n";
+$query = "SELECT * FROM wpc_done ORDER BY done DESC LIMIT 8";
+$res = pg_query($query);
+
+print "<p>Последние запросы</p>\n<table border=\"1\" cellspacing=\"0\">\n<tr><th>Страница</th><th>Дата</th><th>Широта</th><th>Долгота</th></tr>\n";
 while ($row = pg_fetch_assoc($res)) {
-  print "<tr><td>".htmlentities($row["page"])."</td><td>".$row["added"]."</td></tr>\n";
+  print "<tr><td>".htmlentities($row["page"],ENT_COMPAT,"utf-8")."</td><td>".$row["done"]."</td><td>".$row["lat"]."</td><td>".$row["lon"]."</td></tr>\n";
+  print "<tr><td colspan=\"4\">".htmlentities($row["desc"],ENT_COMPAT,"utf-8")."</td></tr>\n";
 }
+
 print "</table>";
+
 ?>
