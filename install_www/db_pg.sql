c3b27ce4e54e2546d5e82cbef87ccf8842969417
ErshKUS <ershkus@gmail.com>
2012-02-21 16:16:26 +0300

    перенёс папку с скриптами установки базы и немного обновил

--- install_www/db_pg.sql
+++ install_www/db_pg.sql
@@ -19,7 +19,7 @@ WITH (
 INSERT INTO "pagedata" ("name", "text", "color", "level", "activate", "order") VALUES
 	( 'map', 'Карта', '#99bd1b', 0, true, 1),
 	( 'cakes', 'Плюшки', '#f9ba1c', 0, true, 2),
-	( 'about', 'О проекте', '#fad051', 0, false, 3),
+	( 'about', 'О проекте', '#fad051', 0, true, 3),
 	( 'contribute', 'Участвовать', '#c3102e', 0, false, 0),
 	( 'news', 'Новости', '#db4c39', 0, false, 0),
 	( 'diaries', 'Дняффки', '#faaa87', 0, false, 0),
