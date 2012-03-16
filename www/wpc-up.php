5cd640e5bc6213a6b299be770085ad6d15c0e320
Alexandr Zeinalov <shurik@sbin.ru>
2012-03-16 21:04:38 +0400

    commons: исправляем баг с дублированием информации из-за использования _ вместо пробела

--- www/wpc-up.php
+++ www/wpc-up.php
@@ -14,6 +14,7 @@ if (isset($_GET["js"]) && $_GET["js"]) $title = urldecode($title);
 $title = preg_replace("@^http://commons\.wikimedia\.org/wiki/@", "", $title);
 $title = preg_replace("@\?.+$@", "", $title);
 $title = preg_replace("@#.+$@", "", $title);
+$title = preg_replace("@_@", " ", $title);
 if (!preg_match("@^File:@", $title)) {
   print "Страница <b>$title</b> не является страницей описания картинки";
   exit;
