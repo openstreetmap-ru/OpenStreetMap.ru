6033f6d701825fef3e1d06652f2da1956bea4612
Larry0ua <pekarskiy.valeriy@gmail.com>
2012-07-06 10:25:33 +0300

    PersonalMap: 30 points limit -> 150

--- www/include/config.php
+++ www/include/config.php
@@ -6,7 +6,7 @@ include_once ('passwd.php');
 include_once ('functions.php');
 include_once ('db.php');
 
-$PERSMAP_MAX_POINTS = 30;
+$PERSMAP_MAX_POINTS = 150;
 $PERSMAP_MAX_LINE_POINTS = 600;
 
 $pages_menu=array(
