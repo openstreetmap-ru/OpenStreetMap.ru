003c8fe6afb660cc2fe1a224c79b16794003195a
Dmitry Panin <panin@yandex-team.ru>
2012-09-30 22:43:20 +0400

    Restored removed file

---
+++ api/db_config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# DATABASE CONFIGURATION
+#
+addr_host = "localhost"
+addr_database = "osm_simple"
+addr_user = "ershkus_work"
+addr_password = "56PUHeCr"
+
+site_host = "localhost"
+site_database = "osmru_web"
+site_user = "ershkus_work"
+site_password = "56PUHeCr"
