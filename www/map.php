03db9045af5e65e84a2fc886d966e340d9809158
ErshKUS <ershkus@gmail.com>
2012-12-02 10:12:09 +0300

    fix #116, прошла конференция

--- www/map.php
+++ www/map.php
@@ -39,9 +39,9 @@ PHP_TOPMENU;
 
 $page_topbar = <<<PHP_TOPBAR
       <div id="newstop">
-        <a href="http://gisconf.ru/ru/" title="Открытые ГИС!">
+        <!--<a href="http://gisconf.ru/ru/" title="Открытые ГИС!">
           <img src="/img/news/OpenGISavatar5eof.png" style="height: 100%;">
-        </a>
+        </a>-->
       </div>
       <div id="searchpan" class="yui3-skin-sam">
         <form id="search" method="get" action="/" onsubmit="return osm.ui.searchsubmit();"><table style="width:100%;"><tr>
