90bd865664ac8c2680894e802b2b76df0de63c8e
ErshKUS <ershkus@gmail.com>
2012-12-11 19:27:28 +0300

    fix opening_hours

--- www/js/poi.js
+++ www/js/poi.js
@@ -179,7 +179,7 @@ osm.poi = {
           .replace(/Su/g, 'Вс')
           .replace('24/7', 'круглосуточно')
           .replace(/off/g, 'не работает')
-          .replace(/\s*;\s*/g, '<br>');
+          .replace(/\s*;\s*/g, ' | ');
       }
       ret = $('<div>').addClass('poi_popup').attr('id',getdata.id)
         .append($('<p>').addClass('poi_header')
