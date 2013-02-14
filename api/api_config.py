00aa557cf268ec09edac7899f6a8b20f2b5f515c
ershkus <ershkus@gmail.com>
2013-02-14 21:24:39 +0300

    вынес настроки api

---
+++ api/api_config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# OpenStreetMap.ru api configuration
+#
+
+# search
+sphinx_host = 'localhost'
+sphinx_port = 3312
+sphinx_index_addr = 'ershkus_search_addr'
+sphinx_index_poi = 'ershkus_poi'
