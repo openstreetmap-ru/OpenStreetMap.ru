6e32a46f8604ef9e1bf585dc0f48291fe06e5dc7
Felis Pimeja <felispimeja+github.com@gmail.com>
2013-04-29 19:52:53 +0400

    A little update for 404 page
    https://github.com/ErshKUS/OpenStreetMap.ru/issues/58

--- www/404.php
+++ www/404.php
@@ -5,6 +5,13 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <body>
-<h3 style="font:28px Arial;">Упс! Ошибка 404.</h3>Такой страницы нет на нашем сайте. Попробуйте поискать на <a href="/">главной странице</a>.
+  <div>
+    <img src="https://dl.dropboxusercontent.com/u/8010189/trash/404.png" alt="Error 404: page not found" style="position:relative; left:50%; margin-left:-75px;">
+    <div style="font-family:'Helvetica Neue', Arial, Helvetica, sans-serif; font-size:1em; color:gray; text-decoration:none; text-align:center; padding:0em;">
+      <p>Упс! Кажется такой страницы не существует.<br>Попробуйте начать с
+        <a href="/">главной страницы</a>
+      </p>
+    </div>
+  </div>
 </body>
 </html>
