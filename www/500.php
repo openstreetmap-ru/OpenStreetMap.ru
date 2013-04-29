09986ebd4296ede3113029ad3fae34ace01b2d16
Felis Pimeja <felispimeja+github.com@gmail.com>
2013-04-29 20:35:50 +0400

    fix for better mobie view

--- www/500.php
+++ www/500.php
@@ -3,6 +3,7 @@
 <head>
 <title>Ошибка 500: Ошибка сервера</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
   <div>
