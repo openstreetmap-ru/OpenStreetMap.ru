64ed78b2c6625fc0bb1a2c710de1fb99d365224d
Petrov Nikolay office <petrovnn@gmail.com>
2019-10-15 17:03:22 +0300

    удалил ссылку на Панорамио

--- www/map.php
+++ www/map.php
@@ -100,7 +100,6 @@ $page_content = <<<PHP_CONTENT
             <li><a id="opento-yandex" href='#' target="_blank">Яндекс карты</a></li>
             <li><a id="opento-wikimapia" href='#' target="_blank">Wikimapia</a></li>
             <li><a id="opento-bing" href='#' target="_blank">Карты Bing</a></li>
-            <li><a id="opento-panoramio" href='#' target="_blank">Panoramio</a></li>
           </ul>
         </div>
       </div>
